cc0b2a961db181c72b5cd519506cce84df858796
Kushagra Gupta <kushagragupta853@gmail.com>
2025-01-30 23:27:55 +0530

    Sorting stack elements program added

---
+++ StackAndQueue/stacksort/SortstackRecursively.java
@@ -0,0 +1,24 @@
+package StackAndQueue.stacksort;
+
+import java.util.*;
+
+public class SortstackRecursively {
+    public static void sortedInsert(Stack<Integer> stack, int element){
+        if(stack.isEmpty() || stack.peek() <= element){
+            stack.push(element);
+        }
+        else{
+            int temp = stack.pop();
+            sortedInsert(stack, element);
+            stack.push(temp);
+        }
+    }
+
+    public static void sortStack(Stack<Integer> stack){
+        if(!stack.isEmpty()){
+            int temp = stack.pop();
+            sortStack(stack);
+            sortedInsert(stack, temp);
+        }
+    }
+}
