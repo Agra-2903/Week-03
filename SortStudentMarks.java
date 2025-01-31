9979b0062bc3593e7eb32d8b96f2e4805b355a42
Kushagra Gupta <kushagragupta853@gmail.com>
2025-01-31 21:32:41 +0530

    Sorting students marks by bubble sort program added

---
+++ SortStudentMarks.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class SortStudentMarks {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break; // Optimization: Stop if no swaps occurred
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] studentMarks = {53, 88, 74, 91, 65};
+        bubbleSort(studentMarks);
+        System.out.println(Arrays.toString(studentMarks));
+    }
+
+}
